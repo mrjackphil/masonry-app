f0b2e9d8e2ef4f4e684306a9a23df1d06dfdf49f
DegtyarEO <geimerus@gmail.com>
2019-02-05 12:49:14 +0300

    Add CREATE_GRID action

--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -1,5 +1,10 @@
-export const CREATE_TILE = 'CREATE_TILE'
+export const CREATE_TILE = "CREATE_TILE";
+export const CREATE_GRID = "CREATE_GRID";
 
 export function createTile(elementData: ITile) {
-  return { type: CREATE_TILE, element: elementData }
+  return { type: CREATE_TILE, element: elementData };
+}
+
+export function createGrid(elements: ITile[]) {
+  return { type: CREATE_GRID, elements: elements };
 }
