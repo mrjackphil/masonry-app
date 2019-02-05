c64737e77417d072b692798fbb24819b82b7a865
DegtyarEO <geimerus@gmail.com>
2019-02-05 12:48:00 +0300

    Add lib for random generations

---
+++ src/lib/random.ts
@@ -0,0 +1,16 @@
+export const random = (min: number, max: number) =>
+  Math.random() * (max - min) + min;
+
+export const randomElement = <T>(ar: T[]) =>
+  ar[Math.floor(Math.random() * ar.length)];
+
+export const randomTile = (
+  minwidth: number,
+  maxwidth: number,
+  colorsArray: string[]
+) => () => {
+  return {
+    height: random(minwidth, maxwidth),
+    color: randomElement(colorsArray)
+  };
+};
