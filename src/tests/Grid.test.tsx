8615ad3e3643e4773ce456f255db62ec12742f3b
DegtyarEO <geimerus@gmail.com>
2019-02-05 12:50:38 +0300

    Refactor Grid.test to work with createGrid function

--- src/tests/Grid.test.tsx
+++ src/tests/Grid.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grid } from '../components/Grid';
-import { createTile } from '../store/actions';
+import { createGrid } from '../store/actions';
 
 it('Grid rendering without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Grid tiles={[]} createTile={createTile}/>, div);
+    ReactDOM.render(<Grid tiles={[]} createGrid={createGrid}/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
