40efc2b412c2c0cb055a3176c98e4fa7b0b6c492
GameDevTeam\MrJackphil <geimerus@gmail.com>
2019-02-05 11:06:12 +0300

    Add grid component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Grid from './components/Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Grid></Grid>
       </div>
     );
   }
