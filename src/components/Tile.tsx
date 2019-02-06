858aeb167da081c842d411abf6b5bfa6ca1e71c4
DegtyarEO <geimerus@gmail.com>
2019-02-06 12:51:37 +0300

    Refactor Tile component to work with ModalTile

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
-import './Tile.css';
+import ModalTile from './ModalTile';
+import './Tile.css'
 
 export default class Element extends Component<ITile> {
-    render() {
-        return (
-            <div className='tile' style={{
-                backgroundColor: this.props.color,
-                height: this.props.height
-            }}></div>
-        );
-    }
+	state: {
+		active: boolean;
+		el: undefined | HTMLDivElement;
+	}
+	constructor(props: ITile) {
+		super(props);
+
+		this.state = {
+			active: false,
+			el: undefined
+		}
+	}
+
+  render() {
+      return (
+        <div>
+          <div
+            className='tile'
+            style={{
+              backgroundColor: this.props.color,
+              height: this.props.height
+            }}
+            onClick={this.click}
+          >
+          </div>
+          { this.state.active && this.state.el &&
+            <ModalTile el={this.state.el} color={this.props.color} height={this.props.height}></ModalTile> }
+        </div>
+      );
+	}
+
+	click = (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    this.setState({active: !this.state.active, el: ev.target});
+	}
 }
