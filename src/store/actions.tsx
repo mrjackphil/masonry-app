bb5fa26c62a1d114ec360f870208169455d4c09b
DegtyarEO <geimerus@gmail.com>
2019-02-06 13:27:08 +0300

    Add open and close tile actions

--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -1,5 +1,7 @@
 export const CREATE_TILE = "CREATE_TILE";
 export const CREATE_GRID = "CREATE_GRID";
+export const OPEN_TILE = "OPEN_TILE";
+export const CLOSE_TILE = "CLOSE_TILE";
 
 export function createTile(elementData: ITile) {
   return { type: CREATE_TILE, element: elementData };
@@ -7,4 +9,12 @@ export function createTile(elementData: ITile) {
 
 export function createGrid(elements: ITile[]) {
   return { type: CREATE_GRID, elements: elements };
+}
+
+export function openTile(elementID: number) {
+  return { type: OPEN_TILE, id: elementID };
+}
+
+export function closeTile(elementID: number) {
+  return { type: CLOSE_TILE, id: elementID };
 }
