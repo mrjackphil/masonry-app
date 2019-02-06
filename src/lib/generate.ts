c9bacd7b373b23de4ea992ffdc47b20eef512867
DegtyarEO <geimerus@gmail.com>
2019-02-06 12:35:56 +0300

    Add id generation for tiles

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,2 +1,15 @@
 export const generateGrid = (limit: number) => <T>(obj: () => T) => (ar: T[] = [] ): T[] =>
-	ar.length < limit ? generateGrid(limit)(obj)(ar.concat([obj()])) : ar;
+	ar.length < limit ? generateGrid(limit)(obj)(ar.concat([addLastId(ar, obj())])) : ar;
+
+const addId = <T>(id: number, obj: T) => {
+	return Object.assign(obj, {id: id});
+}
+
+const getLastId = <T extends {id: number}>(ar: T[]) => {
+	const last = ar[ar.length - 1];
+	return last && typeof last.id !== 'undefined' ? last.id + 1 : 0;
+}
+
+const addLastId = <T>(ar: any[], obj: T) => {
+	return addId( getLastId(ar), obj );
+}
