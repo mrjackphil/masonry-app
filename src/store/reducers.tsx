1444068824981fe242c90bac4cc1724c772b003c
DegtyarEO <geimerus@gmail.com>
2019-02-06 13:27:59 +0300

    Add open and close tile reducers

--- src/store/reducers.tsx
+++ src/store/reducers.tsx
@@ -1,10 +1,17 @@
-import { CREATE_TILE, CREATE_GRID } from './actions'
+import { CREATE_TILE, CREATE_GRID, OPEN_TILE, CLOSE_TILE } from './actions'
 import { combineReducers } from 'redux'
 
 const initialState: ITile[] = [];
 
 function tiles(state = initialState, action: ITileAction) {
   switch (action.type) {
+	case OPEN_TILE:
+		return state.map( el => {
+			return el.id === action.id ? Object.assign(el, {opened: true}) : Object.assign(el, {opened: false});
+		});
+	case CLOSE_TILE:
+		return state
+			.map( el => el.id === action.id ? Object.assign(el, {opened: false}) : el );
     case CREATE_TILE:
       return [
           ...state,
