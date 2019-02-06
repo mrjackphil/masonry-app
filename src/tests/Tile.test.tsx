ffc33935b50f7f291b8b4984ebde54ab76e99198
DegtyarEO <geimerus@gmail.com>
2019-02-06 12:55:41 +0300

    Fix test for Tile with id

--- src/tests/Tile.test.tsx
+++ src/tests/Tile.test.tsx
@@ -4,6 +4,6 @@ import Tile from '../components/Tile';
 
 it('Tile rendering without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Tile height={20} color='red' />, div);
+    ReactDOM.render(<Tile id={0} height={20} color='red' />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
