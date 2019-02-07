8ca35f09268bc5e76b929f7a78ebeffae00c99d4
DegtyarEO <geimerus@gmail.com>
2019-02-07 19:05:23 +0300

    Refactor App checks

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,18 @@ class App extends Component<Props> {
 	  }
   }
 
+  get isModal() {
+	  if (
+		this.props.tiles.filter( tile => tile.loaded).length === 30 &&
+		this.activeTileParams.length === 1 &&
+		this.activeTileElement
+	  ) {
+		return true;
+	  } else {
+		return false;
+	  }
+  }
+
   componentDidMount() {
 	const match = window.location.pathname.match(/\/local\/(\d+)/);
 	if (match && match.length === 2 && Number(match[1])) {
@@ -41,10 +53,12 @@ class App extends Component<Props> {
     return (
       <div className="App">
         {
-			this.props.tiles.filter( tile => tile.loaded).length === 30 &&
-			this.activeTileParams.length === 1 &&
+			this.isModal &&
+			<Overlay></Overlay>
+		}
+		{
+			this.isModal &&
 			this.activeTileElement &&
-			<Overlay></Overlay> &&
             <ModalTile index={this.activeTileParams[0].id}
                        el={this.activeTileElement}
                        color={this.activeTileParams[0].color}
