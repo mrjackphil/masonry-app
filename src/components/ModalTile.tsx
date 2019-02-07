eda2013ae6bb652690df31dfdc383f106b69dff4
DegtyarEO <geimerus@gmail.com>
2019-02-07 17:48:00 +0300

    Fix position for tile animation

--- src/components/ModalTile.tsx
+++ src/components/ModalTile.tsx
@@ -12,68 +12,98 @@ interface Props {
 	closeTile: typeof closeTile;
 }
 
-export class ModalTile extends Component<Props> {
+export class ModalTile extends Component<Props, {actual: any, base: any}> {
 	constructor(props: Props) {
 		super(props);
 		this.state = {
-			backgroundColor: this.props.color,
-			top: this.props.el.getBoundingClientRect().top,
-			left: this.props.el.getBoundingClientRect().left,
-			height: this.props.height,
-			width: '200px'
+			actual: {
+				backgroundColor: this.props.color,
+				top: this.props.el.getBoundingClientRect().top,
+				left: this.props.el.getBoundingClientRect().left,
+				height: this.props.height,
+				width: '200px'
+			},
+			base: {
+				backgroundColor: this.props.color,
+				top: this.props.el.getBoundingClientRect().top,
+				left: this.props.el.getBoundingClientRect().left,
+				height: this.props.height,
+				width: '200px'
+			}
 		}
 	}
 
 	render() {
 		return (
-			<div className='tile-modal' style={ this.state }
-			onClick={this.click}
+			<div className='tile-modal' style={ this.state.actual }
+					onClick={this.click}
 			></div>
 		)
 	}
 
 	hideOverlay = () => {
+		const app = document.querySelector('.App') as HTMLDivElement;
 		const overlay = document.querySelector('.overlay') as HTMLDivElement;
 		if (overlay) {
 			overlay.classList.remove('shown');
 		}
+
+		if (app) {
+			app.classList.remove('hide');
+		}
 	}
 
 	showOverlay = () => {
+		const app = document.querySelector('.App') as HTMLDivElement;
 		const overlay = document.querySelector('.overlay') as HTMLDivElement;
 		if (overlay) {
 			overlay.classList.add('shown');
 		}
+
+		if (app) {
+			app.classList.add('hide');
+		}
 	}
 
 	close = () => {
+		this.hideOverlay();
 		this.setState({
-			backgroundColor: this.props.color,
-			top: this.props.el.getBoundingClientRect().top,
-			left: this.props.el.getBoundingClientRect().left,
-			height: this.props.height,
-			width: '200px'
+			...this.state,
+			actual: this.state.base
 		});
 	}
 
 	open = () => {
+		this.showOverlay();
 		this.setState({
-			top: 0,
-			left: 0,
-			height: '100%',
-			width: '100%',
+			...this.state,
+			actual: {
+				...this.state.actual,
+				top: 0,
+				left: 0,
+				height: '100%',
+				width: '100%',
+			}
 		})
 	}
 
 	componentDidMount = () => {
+		this.setState({
+			...this.state,
+			base: {
+				backgroundColor: this.props.color,
+				top: this.props.el.getBoundingClientRect().top,
+				left: this.props.el.getBoundingClientRect().left,
+				height: this.props.height,
+				width: '200px'
+			}
+		});
 		this.close();
 		this.open();
-		this.showOverlay();
 	}
 
 	click = (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		this.close();
-		this.hideOverlay();
 		const t = ev.target as HTMLDivElement;
 		t.addEventListener('transitionend', () => {
 			this.props.closeTile(this.props.index);
